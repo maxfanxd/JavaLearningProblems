298490676e260847fec4db37678fe942f24025eb
MaxFan1337 <916924308@qq.com>
2023-04-23 18:40:52 +0800

    Initial commit

---
+++ src/Finally.java
@@ -0,0 +1,21 @@
+public class Finally {
+    public static void main(String args[]){
+        int test = tryTest();
+        System.out.println(test);
+    }
+
+    public static int tryTest(){
+        try{
+            System.out.println("正在执行try语句里的第一行");
+            return 100;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return 200;
+        }
+        finally {
+            System.out.println("正在执行finally语句的第一行");
+            return 300;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        float f = 1.0F;
+        System.out.println((i+f)/3);
+        double d = 1.0;
+        System.out.println((i+d)/3);
+        System.out.println((i+f+0.0)/3);
+    }
+}
